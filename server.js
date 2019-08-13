699a1203527a648248c0ad2a0a0e79adb0b792f9
ljinwoo9633 <ljinwoo9633@gmail.com>
2019-08-13 23:07:40 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+
+//routes
+const indexRouter = require('./routes/index');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', () => console.error(error));
+db.once('open', () => console.log(`Connected to Mongoose`));
+
+app.use('/', indexRouter);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT} port`);
+})
