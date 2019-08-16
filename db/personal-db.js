ff227e4b43a46d4db1f654396390a7a0ed3b2083
ljinwoo9633 <ljinwoo9633@gmail.com>
2019-08-16 23:53:36 +0900

    lighter three

---
+++ db/personal-db.js
@@ -0,0 +1,25 @@
+const personal = [
+    //Living
+    {
+        ad_url: "http://aywer.me/t5znhefa8i",
+        icon: "fas fa-passport",
+        word: "여행자보험"
+    },
+    {
+        ad_url: "http://aywer.me/t5znkrtdrd",
+        icon: "fas fa-shower",
+        word: "발포클리너"
+    },
+    {
+        ad_url: "http://joimu.me/t5znksaqq8",
+        icon: "fas fa-comments",
+        word: "채팅 데이톡"
+    },
+    {
+        ad_url: "http://carach.me/t5naij2kmj",
+        icon: "fas fa-comments",
+        word: "은하수다방"
+    }
+]
+
+module.exports = personal;
---
+++ db/success-db.js
@@ -0,0 +1,33 @@
+/*
+{
+    ad_url: "",
+    icon: "",
+    word: ""
+}
+*/
+
+const success = [
+    //education
+    {
+        ad_url: "http://aywer.me/t5znhbfh12",
+        icon: "fas fa-university",
+        word: "경희사이버"
+    },
+    {
+        ad_url: "http://aywer.me/t5znkrr3gq",
+        icon: "fas fa-hand-holding-usd",
+        word: "회계세무"
+    },
+    {
+        ad_url: "http://aywer.me/t5y0bnd37e",
+        icon: "fas fa-shopping-cart",
+        word: "글로 미니"
+    },
+    {
+        ad_url: "http://aywer.me/t5znfpun49",
+        icon: "fas fa-shopping-cart",
+        word: "셀러허브"
+    }
+]
+
+module.exports = success;
