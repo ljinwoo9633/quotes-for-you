f3f67b88a6dfd1e59a6ee26e51c3646a66dbba3c
ljinwoo9633 <ljinwoo9633@gmail.com>
2019-08-17 20:07:18 +0900

    final one comment

--- db/personal-db.js
+++ db/personal-db.js
@@ -1,24 +1,78 @@
 const personal = [
-    //Living
+    {
+        ad_url: "http://donol.me/t5m9vfw0eo",
+        icon: "fas fa-passport",
+        word: "여행자보험"
+    },
+    {
+        ad_url: "http://aywer.me/t5y0bnd37e",
+        icon: "fas fa-shopping-cart",
+        word: "글로 미니가 필요한 순간"
+    },
+    {
+        ad_url: "http://donol.me/t5m9vg1iqq",
+        icon: "fas fa-coins",
+        word: "오토캐쉬백"
+    },
     {
         ad_url: "http://aywer.me/t5znhefa8i",
         icon: "fas fa-passport",
         word: "여행자보험"
     },
     {
+        ad_url: "http://aywer.me/t5znmet2ye",
+        icon: "fas fa-hospital",
+        word: "소람한방병원"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmet2yf",
+        icon: "fas fa-hospital",
+        word: "소람한방병원"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmet2yg",
+        icon: "fas fa-hospital",
+        word: "소람한방병원"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmet2yh",
+        icon: "fas fa-hospital",
+        word: "소람한방병원"
+    },
+    {
+        ad_url: "http://joimu.me/t5zel5g54j",
+        icon: "fas fa-user-injured",
+        word: "KDB 생명보험"
+    },
+    {
+        ad_url: "http://joimu.me/t5znmet2yi",
+        icon: "fas fa-user-injured",
+        word: "KDB 생명보험"
+    },
+    {
+        ad_url: "http://aywer.me/t5znfpun49",
+        icon: "fas fa-shopping-cart",
+        word: "셀러허브"
+    },
+    {
         ad_url: "http://aywer.me/t5znkrtdrd",
         icon: "fas fa-shower",
-        word: "발포클리너"
+        word: "UUU 산소살균 발포클리너"
     },
     {
-        ad_url: "http://joimu.me/t5znksaqq8",
-        icon: "fas fa-comments",
-        word: "채팅 데이톡"
+        ad_url: "http://aywer.me/t5znmexao6",
+        icon: "fas fa-person-booth",
+        word: "http://click.gl/텐핑리그바로가기"
     },
     {
-        ad_url: "http://carach.me/t5naij2kmj",
-        icon: "fas fa-comments",
-        word: "은하수다방"
+        ad_url: "http://aywer.me/t5znmexao7",
+        icon: "fas fa-person-booth",
+        word: "텐핑리그"
+    },
+    {
+        ad_url: "http://aywer.me/t5znkrun1v",
+        icon: "fas fa-smile-beam",
+        word: "유가이즈"
     }
 ]
 
--- db/success-db.js
+++ db/success-db.js
@@ -9,24 +9,74 @@
 const success = [
     //education
     {
+        ad_url: "http://aywer.me/t5znhbfh14",
+        icon: "fas fa-university",
+        word: "경희사이버대학교"
+    },
+    {
         ad_url: "http://aywer.me/t5znhbfh12",
         icon: "fas fa-university",
-        word: "경희사이버"
+        word: "경희사이버대학교"
     },
     {
         ad_url: "http://aywer.me/t5znkrr3gq",
-        icon: "fas fa-hand-holding-usd",
+        icon: "fas fa-file-invoice-dollar",
         word: "회계세무"
     },
     {
-        ad_url: "http://aywer.me/t5y0bnd37e",
-        icon: "fas fa-shopping-cart",
-        word: "글로 미니"
+        ad_url: "http://donol.me/t5znkrr3gp",
+        icon: "fas fa-file-invoice-dollar",
+        word: "세무사 시험"
     },
     {
-        ad_url: "http://aywer.me/t5znfpun49",
-        icon: "fas fa-shopping-cart",
-        word: "셀러허브"
+        ad_url: "http://aywer.me/t5znmet2yj",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmeuan3",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5zncjbihw",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmeuan4",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmet2yk",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5m9w2pr3w",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5znmet2yl",
+        icon: "fas fa-microchip",
+        word: "IT"
+    },
+    {
+        ad_url: "http://aywer.me/t5znkryqnr",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5znkryydf",
+        icon: "fas fa-coins",
+        word: "텐핑"
+    },
+    {
+        ad_url: "http://aywer.me/t5znkryqns",
+        icon: "fas fa-coins",
+        word: "텐핑"
     }
 ]
 
